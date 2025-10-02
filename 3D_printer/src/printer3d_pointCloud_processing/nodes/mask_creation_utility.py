949c7d3815f904178f2c14c84ee860d347f3af70
LoicMosser <mosserl@unistra.fr>
2025-10-02 17:25:59 +0200

    Commit de fin de journée

--- 3D_printer/src/printer3d_pointCloud_processing/nodes/mask_creation_utility.py
+++ 3D_printer/src/printer3d_pointCloud_processing/nodes/mask_creation_utility.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from stl import mesh
-
+from math import sqrt
 
 def intersection_segment_plan(p1, p2, a, b, c, d):
     """Calcule l'intersection entre un segment [p1, p2] et le plan ax + by + cz + d = 0.
@@ -18,6 +18,24 @@ def intersection_segment_plan(p1, p2, a, b, c, d):
         return p1 + t * u
     return None
 
+def find_neighbor(segment, triangle_intersions, tolerance):
+    neighbor = []
+    neighbor_indices = []
+    for j in range(0,len(triangle_intersions)):
+            test_P1_segment_P1_intersection = sqrt((segment[0][0]-triangle_intersions[j][0][0])**2+(segment[0][1]-triangle_intersions[j][0][1])**2) < tolerance
+            test_P1_segment_P2_intersection = sqrt((segment[0][0]-triangle_intersions[j][1][0])**2+(segment[0][1]-triangle_intersions[j][1][1])**2) < tolerance
+            
+            test_P2_segment_P1_intersection = sqrt((segment[1][0]-triangle_intersions[j][0][0])**2+(segment[1][1]-triangle_intersions[j][0][1])**2) < tolerance
+            test_P2_segment_P2_intersection = sqrt((segment[1][0]-triangle_intersions[j][1][0])**2+(segment[1][1]-triangle_intersions[j][1][1])**2) < tolerance
+            
+            if test_P1_segment_P1_intersection != test_P2_segment_P2_intersection:
+                neighbor.append(triangle_intersions[j])
+                neighbor_indices.append(j)
+            if test_P1_segment_P2_intersection != test_P2_segment_P1_intersection:
+                neighbor.append(triangle_intersions[j])
+                neighbor_indices.append(j)
+    return (neighbor, neighbor_indices)
+
 class StlRepresentation():
     def __init__(self, filename):
         self.filename = filename
@@ -30,7 +48,9 @@ class StlRepresentation():
     
     def __identify_sliced_vertices__(self,a,b,c,d):
         intersections = set()
+        triangle_intersions = []
         for triangle in self.vertices:
+            triangle_intersion = []
             # Parcourir les 3 arêtes du triangle
             for i in range(3):
                 p1 = triangle[i]
@@ -38,17 +58,57 @@ class StlRepresentation():
                 # Calculer les valeurs signées de p1 et p2 par rapport au plan
                 val1 = a * p1[0] + b * p1[1] + c * p1[2] + d
                 val2 = a * p2[0] + b * p2[1] + c * p2[2] + d
-                print("val1 * val2 : ",val1 * val2)
                 # Si les signes sont opposés, il y a intersection
                 if val1 * val2 < 0:
-                    print("val1 * val2")
                     intersection = intersection_segment_plan(p1, p2, a, b, c, d)
                     if intersection is not None:
                         intersections.add(tuple(intersection))
-        return intersections
+                        triangle_intersion.append(tuple(intersection))
+            if len(triangle_intersion) >0:
+                triangle_intersions.append(triangle_intersion)
+        return (intersections,triangle_intersions)
 
-    def slice_stl_representation(self, plane_parameters):
+    def slice_stl_representation(self, a,b,c,d):
+        (intersections,triangle_intersions) = self.__identify_sliced_vertices__(a,b,c,d)
+        intersections = list(intersections)
+        intersections_x = [intersections[i][0] for i in range(0, len(intersections))]
+        intersections_y = [intersections[i][1] for i in range(0, len(intersections))]
+        
+        contours = {}
+        
+        contours[1] = [triangle_intersions[0]]
+        (neighbor, neighbor_indices) = find_neighbor(triangle_intersions[0], triangle_intersions, 0.001)
+        for i in range(0,len(neighbor)):
+            contours[1].append(neighbor[i])
+        test = False
+        while test == False:
+            indices_to_be_tested = neighbor_indices.copy()
+            neighbor_indices = [None]*len(indices_to_be_tested)
+            neighbor = [None]*len(indices_to_be_tested)
+            for i in range(0,len(indices_to_be_tested)):
+                (neighbor[i], neighbor_indices[i]) = find_neighbor(triangle_intersions[0], triangle_intersions, 0.001)
+        
+        
+        
+        
+        plt.figure(figsize=(16, 12))
+        plt.plot(intersections_x,intersections_y,'r*')
+        for i in range(0,len(contours[1])):
+            plt.plot([contours[1][i][0][0], contours[1][i][1][0]],
+                     [contours[1][i][0][1], contours[1][i][1][1]],
+                     'b-')
+        plt.plot([triangle_intersions[0][0][0], triangle_intersions[0][1][0]],
+                 [triangle_intersions[0][0][1], triangle_intersions[0][1][1]],
+                 'g-')
+        plt.axis("equal")
+        plt.show()
+        
+        
         return 0
+    
+    def plot_sliced_stl(self,a,b,c,d):
+        return 0
+            
 
     def get_layer_internal_contour(self, stl_slice):
         return 0
@@ -58,5 +118,5 @@ class StlRepresentation():
     
 if __name__ == '__main__':
     print("demarrage")
-    STL_representation = StlRepresentation("/home/gulltor/Téléchargements/example_cylinder.stl")
-    print(STL_representation.__identify_sliced_vertices__(0,0,1,-25))
+    STL_representation = StlRepresentation("/home/mosserloic/Documents/example_cylinder.stl")
+    STL_representation.slice_stl_representation(0,0,1,-25)
